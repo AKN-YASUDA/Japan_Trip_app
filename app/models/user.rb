45c4babcc371c359e38185399fa39a2712ef36c7
AKN-YASUDA <hbbpf766@yahoo.co.jp>
2021-04-17 11:59:42 +0900

    user modelカラムのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_option presence: true do
+    validates :password, format: { with: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/ }
+    validates :nickname
+    validates :name, format: { with: /\A[ぁ-んァ-ン\p{Han}]/ }
+    validates :read_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_year
+    validates :birth_month
+    validates :birth_day
+  end
+  has_many :diaries
+  has_many :comments
 end
